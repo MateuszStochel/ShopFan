90a4612c4d2c311c4776b8925e3f582b0d612253
Mateusz Stochel <mateuszstochel1995@gmail.com>
2021-03-07 23:48:03 +0100

    Deleting carousel from homepage

--- src/PromoProducts.js
+++ src/PromoProducts.js
@@ -3,44 +3,6 @@ import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import styled from "styled-components";
 
-const responsive = {
-  xxxl: {
-    breakpoint: { max: 5000, min: 1600 },
-    partialVisibilityGutter: 80,
-    items: 4,
-  },
-  xxl: {
-    breakpoint: { max: 1600, min: 1400 },
-    partialVisibilityGutter: 10,
-    items: 4,
-  },
-  xl: {
-    breakpoint: { max: 1400, min: 1024 },
-    partialVisibilityGutter: 40,
-    items: 3,
-  },
-  lg: {
-    breakpoint: { max: 1024, min: 800 },
-    partialVisibilityGutter: 100,
-    items: 2,
-  },
-  m: {
-    breakpoint: { max: 800, min: 600 },
-    partialVisibilityGutter: 10,
-    items: 2,
-  },
-  s: {
-    breakpoint: { max: 600, min: 480 },
-    partialVisibilityGutter: 100,
-    items: 1,
-  },
-  xs: {
-    breakpoint: { max: 400, min: 0 },
-    partialVisibilityGutter: 40,
-    items: 1,
-  },
-};
-
 const Wrappers = styled.div``;
 const Image = styled.img`
   width: 2000px;
@@ -53,13 +15,7 @@ const PromoProducts = ({ products }) => {
       <Image src={image} />
     </Wrappers>
   ));
-  return (
-    <PromoProductsWrapper>
-      <Carousel partialVisbile responsive={responsive}>
-        {carouselSlides}
-      </Carousel>
-    </PromoProductsWrapper>
-  );
+  return <PromoProductsWrapper></PromoProductsWrapper>;
 };
 
 export default PromoProducts;
